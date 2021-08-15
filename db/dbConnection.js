14067d9fc9c6861c59130525ace3595b47ab6d38
Mohammad Haseeb Ashiq <learndotpk@gmail.com>
2021-08-15 07:08:52 -0700

    db push

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose=require('mongoose');
 
 exports.init=()=>{
-    mongoose.connect(process.env.dbLocal,
+    mongoose.connect("mongodb+srv://haseeb-ashiq:j1A6L5ls8IsK7d29@cluster0.6dnqg.mongodb.net/taxibooking?retryWrites=true&w=majority",
         { useCreateIndex: true,
             useFindAndModify: false, 
             useNewUrlParser: true, 
