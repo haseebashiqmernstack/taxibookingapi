bbf5813c93130319d7bfef9e3f6786889703a97f
umair-riaz-official <41395451+umair-riaz-official@users.noreply.github.com>
2021-08-15 19:16:25 +0500

    port resolved

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ apps.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
 }).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+    console.log('App is running, server is listening on port ', apps.get('port'));
 });
 
 
