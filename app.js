26930d510f827fc682e60710c8ce6c340d80ee07
umair-riaz-official <41395451+umair-riaz-official@users.noreply.github.com>
2021-08-15 19:14:17 +0500

    env

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 require('dotenv').config();
-const express=require('express');
-const cors=require('cors');
-const cookieParser=require('cookie-parser');
-const driverRouter=require('./routers/driverRouter');
-const bookingRouter=require('./routers/bookingRouter');
-const riderRouter=require('./routers/riderRouter');
-const vehicleRouter=require('./routers/vehicleRouter');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const driverRouter = require('./routers/driverRouter');
+const bookingRouter = require('./routers/bookingRouter');
+const riderRouter = require('./routers/riderRouter');
+const vehicleRouter = require('./routers/vehicleRouter');
 require('./db/dbConnection').init();
-const apps=express();
-const port=process.env.PORT | 5000;
+const apps = express();
+const port = process.env.PORT || 5000;
 apps.use(cors());
 apps.use(express.json());
 apps.use(cookieParser());
 
-apps.use('/api',driverRouter);
-apps.use('/api',bookingRouter);
-apps.use('/api',riderRouter);
-apps.use('/api',vehicleRouter);
-apps.listen(port,()=>console.log(`server is running on ${port}`));
+apps.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+
+apps.use('/api', driverRouter);
+apps.use('/api', bookingRouter);
+apps.use('/api', riderRouter);
+apps.use('/api', vehicleRouter);
+apps.listen(port, () => console.log(`server is running on ${port}`));
